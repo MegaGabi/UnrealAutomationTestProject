13243e27df1f7f332196a92c6e766a404b342acf
Artem Yakovenko2 <artem_yakovenko2@epam.com>
2024-07-04 12:53:18 +0200

    Initial import

---
+++ Build.bat
@@ -0,0 +1,6 @@
+"D:\UE_src\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project="D:\Unreal Projects\TestProject\TestProject.uproject" ^
+-platform=Win64 -clientconfig=Development ^
+-archivedirectory="D:\Unreal Projects\TestProject\BuildFromBat" ^
+-build
